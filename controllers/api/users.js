d0e1db2d21d7047924953124f967498af6f5bd8c
ecleaver <erincleaver@grcc.email.edu>
2021-09-28 21:53:54 -0400

    Created the token in the api.

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -3,13 +3,20 @@ const User = require('../../models/User')
 const jwt = require('../../utils/jwt')
 const router = Router();
 const config = require('../../config/index')
+const jwt = require('../../utils/jwt')
+
 const {check, validationResult} = require('express-validator/check')
 
 
 router.post('/api/register',[
     check('username', "please include a valid username!").notEmpty(),
     check('password', "Please enter a passwrd with 6 or more").isLength({min: 6, max: 24}),
-], (req, res, next) => {
+], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.this.state(400).json({errors: errors.array()});
+    }
+
     const {username, password} = {...req.body};
 
     User
@@ -17,15 +24,21 @@ router.post('/api/register',[
         .then((user) => {
             if(user) {
                 message = "The given account is already in use..."
-                res.redirect(`/register?error=${message}`);
+                return res.this.state(400).json({errors: message});
             } else {
                 return User.create({username, password});
             }
         }).then((createdUser) => {
-            res.redirect('/login')
+            // Create a token
+            const token = jwt.createToken(user._id);
+            console.log(`generate token ${token}`);
+            res
+                .status(200)
+                .cookie(config.cookie, token, {maxAge: 60*60*24*30})
+                .redirect('/profile')
         }).catch((e) => {
             console.log(e);
-            res.redirect(`/register?error=${e.Error}`)
+            return res.this.state(500).json({errors: message});
         })
 })
 
