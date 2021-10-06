c7ea259dbb5a12712c749b84ef12729af3998028
ecleaver <erincleaver@grcc.email.edu>
2021-10-05 20:33:09 -0400

    Removed comments

--- server/index.js
+++ server/index.js
@@ -29,42 +29,4 @@ async function loadPets() {
     return await response.json();
 }
 
-/*
-app.get('/pets', (req, res) => {
-    res.status(200);
-    res.json(pets);
-})
-
-app.get('/loadpets', async(req, res) => {
-    res.status(200);
-    console.log(pets2)
-    res.json(pets2)
-})
-
-app.get('/', async(req, res) => {
-    res.status(200);
-    let pets2 = await loadPets();
-    res.render('home', { pets: pets2 });
-})
-
-app.get('/edit', (req, res) => {
-    res.status(200);
-    res.render('edit');
-})
-
-app.get('/delete', (req, res) => {
-    res.status(200);
-    res.render('delete');
-})
-
-app.get('/AddPet', (req, res) => {
-    res.status(200);
-    res.render('addPet');
-})
-
-app.get('/AddBreed', (req, res) => {
-    res.status(200);
-    res.render('addBreed', { name: "" });
-})*/
-
 app.listen(config.development.port, () => console.log("Running server"));
